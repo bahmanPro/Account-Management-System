f98afb669bfef36d270612ff8120e308a1947f49
bahmanPro <65077685+bahmanPro@users.noreply.github.com>
2020-05-09 17:22:23 +0300

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NULL, 
+    [fullname] VARCHAR(50) NULL, 
+    [email] VARCHAR(100) NULL, 
+    [phone] VARCHAR(11) NULL, 
+    [password] VARCHAR(50) NULL, 
+    [type] VARCHAR(50) NULL, 
+    [url] VARCHAR(100) NULL, 
+    [question] VARCHAR(100) NULL, 
+    [note] VARCHAR(100) NULL
+)
